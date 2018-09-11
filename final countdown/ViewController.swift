d10c8f30c1489d1e4408b0a27302064ad76bf2fc
“Zorro” <ekwzorrowing@gmail.com>
2018-09-11 18:40:36 +0800

    Initial Commit

---
+++ final countdown/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  final countdown
+//
+//  Created by 王奕功 on 2018/9/11.
+//  Copyright © 2018年 王奕功. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
